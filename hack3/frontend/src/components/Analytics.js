d0b59cd3b90b32b71e8b57eeeb33da85d5caed6b
李昀宸 <b09705015@ntu.edu.tw>
2022-12-20 10:10:44 +0800

    hack3

--- hack3/frontend/src/components/Analytics.js
+++ hack3/frontend/src/components/Analytics.js
@@ -8,11 +8,8 @@ import Category from "./Category";
 function Analytics() {
   // TODO 2.2 Use the useQuery hook to get items from backend
   const { data: itemsData } = useQuery(GET_ITEMS_QUERY);
-  if (data) {
-    const { items } = itemsData;
-  } else {
-    const { items } = {};
-  }
+  let items = {};
+  if (itemsData) items = itemsData.items;
 
   // TODO 2.2 End
 
