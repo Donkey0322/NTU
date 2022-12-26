fdb4822388f4d68f46b2edc0fc113868d8b348b3
Lichi0404 <lichi10404@gmail.com>
2022-12-27 02:43:53 +0800

    終於修好

--- backend/src/routes/customers.js
+++ backend/src/routes/customers.js
@@ -2,52 +2,41 @@ import db from "../sql.js";
 import express from 'express';
 import moment from "moment";
 
-// router.post('/', async (req, res) => {
-//     console.log(req.body);
-//     var add = await addCustomer(req.body);
-//     res.json({ message: `${add} (${req.body.name}, ${req.body.subject}, ${req.body.score})`, card: true});
-// });
 
 const router = express.Router();
-const queryCustomer = async () => {
-    let query = `select * from customers
-                        order by customer_id desc;`;
-
-    let table = await db.query(query, function(err, result) {
-        if(err) throw err;
-        else{
-            console.log('Customer query done')
-            return result;
-        }
-    }); 
-    table.map((element) => {
-        element.birthday = moment(element.birthday).utc().format('YYYY-MM-DD')
+const Myquery = (query) => {
+    return new Promise((resolve) => {
+        db.query(query,  (err, result) => {
+            if (err) {
+                throw err;
+            }else{
+                result.map((element) => {
+                    element.birthday = moment(element.birthday).utc().format('YYYY-MM-DD')
+                    element.birthday = new Date(element.birthday)
+                })
+                resolve(result);
+            }
+        })
     })
-    return table;
-};
-
-const deleteCustomer = async(data) => {
-    let id = data;
-    let query = `delete from customers
-                 where customer_id = ${id}`;
-    await db.query(query, function(err, result) {
-    if(err) throw err;
-    else{
-        console.log('Customer delete done')
-    }
-    });
 }
 
 router.delete('/', async (req, res) => {
     // console.log(req.body);
     let id = req.query
-    await deleteCustomer(id);
-    let result = await queryCustomer();
-    res.json({ result });
+    let query = `delete from customers
+                 where customer_id = ${id}`;
+    await Myquery(query)
+    let return_query = `select * from customers;`;
+    var result = await Myquery(return_query)
+    res.status(200).send({result})
 });
 
 router.get("/", async (_, res) => {
-    let result = await queryCustomer();
-    res.json({ result });
+    let query = `select * from customers
+    order by customer_id desc;`;
+    var result = await Myquery(query)
+    res.status(200).send({result})
+    
+    // res.json({ result });
 });
 export default router;
--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -37,16 +37,27 @@ const make_dict = (array_c, detail) => {
 //     let n
 // };
 
-const deleteOrder = async(data) => {
-    let id = data;
-    let query = `delete from orders
-                 where order_id = ${id}`;
-    await db.query(query, function(err, result) {
-    if(err) throw err;
-    else{
-        console.log('Order delete done')
-    }
-    });
+
+const Myquery = (query, detail) => {
+    return new Promise((resolve) => {
+        db.query(query,  (err, result) => {
+            if (err) {
+                throw err;
+            }else{
+                if(!detail){
+                    result.map((element) => {
+                        element.order_date = moment(element.order_date).utc().format('YYYY-MM-DD')
+                        element.order_date = new Date(element.order_date)
+                        if(element.deliver_date !== null){
+                            element.deliver_date = moment(element.deliver_date).utc().format('YYYY-MM-DD')
+                            element.deliver_date = new Date(element.deliver_date)
+                        }
+                        })
+                }
+            }
+            resolve(result);
+        })
+    })
 }
 
 const queryOrder = async () => {
@@ -56,32 +67,18 @@ const queryOrder = async () => {
                         left join orders_detail on orders.order_id = orders_detail.order_id
                         left join products on orders_detail.product_id = products.product_id
                         left join customers on customers.customer_id = orders.customer
-                        left join promotions on promotions.promotion_id = orders.promotion
                         group by orders.order_id
                         order by deliver_date is not null, deliver_date desc;`;
     let query_detail = `select orders.order_id, products.product_name, orders_detail.quantity, (orders_detail.quantity * products.price) as money from orders
                         left join orders_detail on orders.order_id = orders_detail.order_id
                         left join products on orders_detail.product_id = products.product_id
                         left join customers on customers.customer_id = orders.customer
-                        left join promotions on promotions.promotion_id = orders.promotion
                         order by deliver_date is not null, deliver_date desc;`
 
-    let array1 = await db.query(query_origin, function(err, result) {
-        if(err) throw err;
-        return result;
-    }); 
-    array1.map((element) => {
-        element.order_date = moment(element.order_date).utc().format('YYYY-MM-DD')
-        if(element.deliver_date !== null){
-            element.deliver_date = moment(element.deliver_date).utc().format('YYYY-MM-DD')
-        }
-    })
-    let origin = make_dict(array_c = array1, detail = false)
-    let array2 = await db.query(query_detail, function(err, result) {
-        if(err) throw err;
-        return result;
-    }); 
-    let detail = make_dict(array_c = array2, detail = true)
+    let array1 = await Myquery(query_origin, false)
+    let origin = make_dict(array1, false)
+    let array2 = await Myquery(query_detail, true)
+    let detail = make_dict(array2, true)
     var dict = {origin, detail}
     return dict
 };
@@ -89,13 +86,16 @@ const queryOrder = async () => {
 router.delete('/', async (req, res) => {
     // console.log(req.body);
     let id = req.query
-    await deleteOrder(id);
-    let result = await queryOrder();
-    res.json({ result });
+    let query = `delete from orders
+                 where order_id = ${id}`;
+    await Myquery(query, true);
+    var result = await queryOrder();
+    res.status(200).send({result});
 });
 
 router.get("/", async (_, res) => {
-    let result = await queryOrder();
-    res.json({ result });
+    var result = await queryOrder();
+    console.log(result)
+    res.status(200).send({result});
 });
 export default router;
