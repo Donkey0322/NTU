53f7e97aa42d6747915b915f562abafba8010283
李昀宸 <b09705015@ntu.edu.tw>
2022-12-27 12:30:17 +0800

    我的

--- frontend/src/hooks/useDB.js
+++ frontend/src/hooks/useDB.js
@@ -34,8 +34,10 @@ const DBProvider = (props) => {
             const {
               data: { result },
             } = await instance.post(`${path}`, value);
-            console.log(table, result)
-            setTable([...result, ...table]);
+            console.log(table, result);
+            if (!result) {
+              setTable([...result, ...table]);
+            }
             break;
           } catch (error) {
             // return new Promise((resolve, reject) => {
@@ -64,7 +66,7 @@ const DBProvider = (props) => {
                 tuple[indexName] === result[0][indexName] ? result[0] : tuple
               );
             }
-            console.log(newResult)
+            console.log(newResult);
             setTable(newResult);
             break;
           } catch (error) {
